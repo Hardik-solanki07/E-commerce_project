6524201a1c26dc10f4f8627704a31b2c26f8fbe9
Hardik-solanki07 <hardiksolanki37090@gmail.com>
2023-09-26 15:55:31 +0530

    py

---
+++ dict.py
@@ -0,0 +1,36 @@
+# a={
+#     "name":"hardik",
+#     "age":22,
+#     "sub":"python",
+# }
+# print(a)
+# print(type(a))
+
+# a = { 'Divyesh':{'hight' : 168, 'weight':125} , 'Sahil':{'hight' : 195, 'weight':58}}
+# A=a['Divyesh']['weight']
+
+# print(A)
+    
+    
+# s="my name is hardik"
+# c=0
+# a=0
+# for i in s:
+#     if i=="m":
+#         c+=1
+#     elif i=="y":
+#         a+=1
+# print(c)
+# print(a)
+
+d={'ravi': 10, 'rajnish': 9, 'sanjeev': 15, 'yash': 2, 'suraj': 32}
+
+# a=list(d.keys())
+# a.sort()
+# # print(a)
+# for i in a:
+#     print(d[i])
+
+
+
+
